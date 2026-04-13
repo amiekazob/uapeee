d9b912d42543f0c1241573d3718d56ab6e7eaa3a
Md Samiul Alam Shajal <97245391+amiekazob@users.noreply.github.com>
2026-04-13 21:37:43 +0600

    Rename 'POE' link to 'PEOs' in navbar

--- components/layout/navbar.tsx
+++ components/layout/navbar.tsx
@@ -104,7 +104,7 @@ export function Navbar() {
                         Mission & Vision
                       </LoadingLink>
                       <LoadingLink href="/poe" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-50">
-                        POE
+                        PEOs
                       </LoadingLink>
                     </div>
                   </div>
